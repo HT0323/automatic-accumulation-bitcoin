94923a80df4e9bb1d3ecebfe9716d294837b0838
HaraT0323 <alra9si916323@gmail.com>
2022-11-13 13:18:23 +0900

    System MangerからAPIKey情報を取得

--- buy-btc/go.mod
+++ buy-btc/go.mod
@@ -1,4 +1,7 @@
-require github.com/aws/aws-lambda-go v1.23.0
+require (
+	github.com/aws/aws-lambda-go v1.23.0
+	github.com/aws/aws-sdk-go v1.44.136 // indirect
+)
 
 replace gopkg.in/yaml.v2 => gopkg.in/yaml.v2 v2.2.8
 
--- buy-btc/main.go
+++ buy-btc/main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
-	"buy-btc/bitflyer"
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ssm"
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	ticker, err := bitflyer.GetTicker(bitflyer.Btcjpy)
+	// ticker, err := bitflyer.GetTicker(bitflyer.Btcjpy)
+
+	// apiKey, err := getParameter("buy-btc-apikey")
+	// if err != nil {
+	// 	return getErrorResponse(err.Error()), nil
+	// }
+
+	apiSecret, err := getParameter("buy-btc-apisecret")
+	if err != nil {
+		return getErrorResponse(err.Error()), nil
+	}
+
 	if err != nil {
 		return events.APIGatewayProxyResponse{
 			Body:       "Bad Request!!!",
@@ -18,11 +31,37 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	}
 
 	return events.APIGatewayProxyResponse{
-		Body:       fmt.Sprintf("Ticker:%+v", ticker),
+		Body:       fmt.Sprintf("ApiKey:%+v", apiSecret),
 		StatusCode: 200,
 	}, nil
 }
 
+// System Mangerからパラメータを取得
+func getParameter(key string) (string, error) {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	svc := ssm.New(sess, aws.NewConfig().WithRegion("ap-northeast-1"))
+
+	params := &ssm.GetParameterInput{
+		Name:           aws.String(key),
+		WithDecryption: aws.Bool(true),
+	}
+	res, err := svc.GetParameter(params)
+	if err != nil {
+		return "", err
+	}
+
+	return *res.Parameter.Value, nil
+}
+
+func getErrorResponse(message string) events.APIGatewayProxyResponse {
+	return events.APIGatewayProxyResponse{
+		Body:       message,
+		StatusCode: 400,
+	}
+}
 func main() {
 	lambda.Start(handler)
 }
